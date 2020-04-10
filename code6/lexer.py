c3b73ec0a079952cf4324a2f0b1bc1e0d1bb60e9
jhlee838 <jhlee838@gmail.com>
2020-04-10 13:29:58 +0900

    a

--- code6/lexer.py
+++ code6/lexer.py
@@ -1,11 +1,36 @@
 import re
 import numpy as np
+import ply.lex as lex
+import sys
 
-s = open("main.ion", 'r').read()
-o = open("index.html", 'a')
+tokens = [
+    'TAB',
+    'COLON',
+    'EQUAL',
+    'SPACE'
+]
 
-arr = []
-tokens = []
+t_COLON = r':'
+t_EQUAL = r'='
 
-s = re.split("[\t]*", s)
-print(s)
+def t_TAB(t):
+    r'\t'
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_error(t):
+    print("Error on token %s" % t.value[0])
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+
+lexer.input("a")
+
+while True:
+    tok = lexer.token()
+    if not tok:
+        break
+    print(tok)

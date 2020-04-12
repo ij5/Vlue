ed8676643473ed0108730a3294b9de4aa5497766
jhlee838 <jhlee838@gmail.com>
2020-04-12 12:44:05 +0900

    add files

--- code2/lexer.c
+++ code2/lexer.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <ctype.h>
 
-enum Lexicon {WHITESPACE, IDENTIFIER, PLUS, LEFT_BRACE, RIGHT_BRACE};
+enum Lexicon {WHITESPACE, IDENTIFIER, PLUS, LEFT_BRACE, RIGHT_BRACE, END};
 
 struct Token {
     enum Lexicon lex;
@@ -48,12 +49,140 @@ struct Token lexer(const char **str) {
     return token;
 }
 
+enum Syntax {SYN_EXPRESSION, SYN_PLUS, SYN_CALL, SYN_IDENTIFIER};
+
+struct AbstractSyntaxTree {
+    enum Syntax syn;
+    struct AbstractSyntaxTree* left;
+    struct AbstractSyntaxTree* right;
+    const char *data;
+};
+
+struct AbstractSyntaxTree* expr2(struct Token **t);
+struct AbstractSyntaxTree* expr1_prime(struct Token **t);
+struct AbstractSyntaxTree* parser(struct Token **t); //expr1
+
+struct AbstractSyntaxTree* expr3(struct Token **t)
+{
+    struct AbstractSyntaxTree* node = malloc(sizeof(*node));
+    if ((*t)->lex == IDENTIFIER) {
+        if ((*t)->len == 1) {
+            char *data = malloc((*t)->len+1);
+            strncpy(data, (*t)->str, (*t)->len);
+            data[(*t)->len] = '\0';
+            ++*t; // matching identifier
+            node->left = NULL;
+            node->right = NULL;
+            node->syn = SYN_IDENTIFIER;
+            node->data = data;
+        } else {
+            printf("error : variable can be a one letter.\n");
+            exit(0);
+        }
+    } else {
+        printf("syntax error(3) : not a identifier. \n");
+        exit(0);
+    }
+    return node;
+}
+
+struct AbstractSyntaxTree* expr2(struct Token **t)
+{
+    struct AbstractSyntaxTree* node = malloc(sizeof(*node));
+    if ((*t)->lex == IDENTIFIER) {
+        if ( ((*t)+1)->lex == LEFT_BRACE) {
+            char *data = malloc((*t)->len+1);
+            strncpy(data, (*t)->str, (*t)->len);
+            data[(*t)->len] = '\0';
+            ++*t; // matching 'identifier'
+            ++*t; // matching '['
+            node->left = parser(t); // expr1
+            node->right = NULL;
+            node->data = data;
+            node->syn = SYN_CALL;
+        } else {
+            free(node);
+            node = expr3(t);
+        }
+    } else {
+        printf("syntax error(2) : not a identifier nor calling expression.\n");
+        exit(0);
+    }
+    return node;
+}
+
+struct AbstractSyntaxTree* expr1_prime(struct Token **t)
+{
+    struct AbstractSyntaxTree* node = malloc(sizeof(*node));
+    if ((*t)->lex == PLUS) {
+        ++*t; // match
+        node->left = expr2(t);
+        node->right = expr1_prime(t);
+        node->data = NULL;
+        node->syn = SYN_PLUS;
+    } else  if ((*t)->lex == RIGHT_BRACE || (*t)->lex == END) {
+        ++*t;
+        free(node);
+        node = NULL;
+    } else {
+        printf("syntax error(1) : not a plus nor end of expression.\n");
+        exit(0);
+    }
+    return node;
+}
+struct AbstractSyntaxTree* parser(struct Token **t) //expr1
+{
+    struct AbstractSyntaxTree* node = malloc(sizeof(*node));
+    if ((*t)->lex == IDENTIFIER) {
+        node->syn = SYN_EXPRESSION;
+        node->left = expr2(t);
+        node->right = expr1_prime(t);
+        node->data = NULL;
+    } else {
+        printf("syntax error!(0) : not a expression.\n");
+        exit(0);
+    }
+    return node;
+}
+
+double variable[256] = {0};
+
+double evaluate(struct AbstractSyntaxTree* node) {
+    double val;
+    if (node->syn == SYN_EXPRESSION || node->syn == SYN_PLUS) {
+        val = evaluate(node->left);
+        if (node->right != NULL) {
+            if (node->right->syn == SYN_PLUS) {
+                val += evaluate(node->right);
+            }
+        }
+    } else if (node->syn == SYN_CALL) {
+        if (strcmp(node->data, "cos") == 0) {
+            val = cos(evaluate(node->left));
+        } else if (strcmp(node->data, "sin") == 0) {
+            val = sin(evaluate(node->left));
+        } else if (strcmp(node->data, "tan") == 0) {
+            val = tan(evaluate(node->left));
+        }
+    } else if (node->syn == SYN_IDENTIFIER) {
+        val = variable[*(node->data)];
+    }
+    return val;
+}
+
 int main(void) {
-    const char *input = "cos(t) + sin(f)";
+    char input[256] = {0};
     const char *str = input;
     struct Token tokens[1024];
     int number_tokens = 0;
     int i;
+    struct Token *p = tokens;
+    struct AbstractSyntaxTree *root;
+    double result;
+
+    printf("you can use \"+\", \"cos(), sin(), tan()\", \"t\"(30degree) and |\"f\"(60degree)\n");
+    printf(" input a expreesion :");
+    scanf("%254s", input);
 
     while(str < &input[strlen(input)]) {
         struct Token token = lexer(&str);
@@ -61,12 +190,18 @@ int main(void) {
             tokens[number_tokens++] = token;
         }
     }
+    tokens[number_tokens++].lex = END;
 
-    // print all tockens for test
-    for(i=0; i<number_tokens; ++i) {
-        const char* message [] = {"whitespace", "identifier", "+", "(", ")"};
-        printf("%s\n", message[tokens[i].lex]);
-    }
+    root = parser(&p);
 
+    // t = 30 degree, f = 60 degree
+    variable['t'] = 30.0 * 3.141592/180.0;
+    variable['f'] = 60.0 * 3.141592/180.0;
+    // calculate !!
+    result = evaluate(root);
+
+    printf("result : %f\n", result);
+
+    // free all of node and data about root.
     return 0;
 }
--- code6/lexer.py
+++ code6/lexer.py
@@ -1,5 +1,51 @@
-###################
-#####TOKENS########
-###################
+from ply import lex
+from ply import yacc
 
-T_EQUAL = 'EQUAL'
+tokens = (
+	'LSB',
+	'RSB',
+	'LMB',
+	'RMB',
+	'IDENTIFIER',
+	'EQUAL',
+	'CONTENTS'
+)
+
+t_LSB = r'\('
+t_RSB = r'\)'
+t_LMB = r'\{'
+t_RMB = r'\}'
+t_EQUAL = r'\='
+
+def t_IDENTIFIER(t):
+	r'\w'
+	return t
+
+def t_newline(t):
+	r'\n+'
+	t.lexer.lineno += len(t.value)
+
+t_ignore = ' \t'
+
+def t_error(t):
+	print("error on token %s" % t.value)
+	t.lexer.skip(1)
+
+lexer = lex.lex()
+
+data = '''
+html(){
+	head(){}
+	body(a=asd){
+
+	}	
+}
+'''
+
+lexer.input(data)
+
+while True:
+	tok = lexer.token()
+	if not tok:
+		break
+	print(tok)

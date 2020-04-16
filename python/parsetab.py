a336f94ff4cc8e1ce0c29f7b1cbd6e958652daac
jhlee838 <jhlee838@gmail.com>
2020-04-16 16:22:36 +0900

    update files

--- python/parsetab.py
+++ python/parsetab.py
@@ -6,7 +6,7 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'A BODY COMMA DIV EQUAL HEAD HTML IDENTIFIER LMB LSB OTHER RMB RSBhead_expr : HTML elements_outside\n    elements_outside : LSB elements_inside_comma RSB\n    \n    elements_inside_comma : elements_inside_equal COMMA elements_inside_equal\n    \n    elements_inside_comma : elements_inside_equal\n        | empty\n    \n    elements_inside_equal : attr_root EQUAL attr_root\n    \n    attr_root : attr attr\n    attr_root : attr\n    attr : attr IDENTIFIER\n        | attr OTHER\n    \n    attr : IDENTIFIER\n        | OTHER\n    empty : '
+_lr_signature = 'A BODY COMMA DIV EQUAL HEAD HTML IDENTIFIER LMB LSB OTHER RMB RSB\n    head_expr : HTML elements_outside\n    \n    elements_outside : LSB elements_inside_comma RSB\n    \n    elements_inside_comma : elements_inside_equal COMMA elements_inside_equal\n    \n    elements_inside_comma : elements_inside_equal\n        | empty\n    \n    elements_inside_equal : attr_root EQUAL attr_root\n    \n    attr_root : attr attr\n    attr_root : attr\n    attr : attr IDENTIFIER\n        | attr OTHER\n    \n    attr : IDENTIFIER\n        | OTHER\n    empty : '
     
 _lr_action_items = {'HTML':([0,],[2,]),'$end':([1,3,12,],[0,-1,-2,]),'LSB':([2,],[4,]),'RSB':([4,5,6,7,9,10,11,15,16,17,18,19,20,21,],[-13,12,-4,-5,-8,-11,-12,-7,-9,-10,-3,-6,-9,-10,]),'IDENTIFIER':([4,9,10,11,13,14,15,16,17,20,21,],[10,16,-11,-12,10,10,20,-9,-10,-9,-10,]),'OTHER':([4,9,10,11,13,14,15,16,17,20,21,],[11,17,-11,-12,11,11,21,-9,-10,-9,-10,]),'COMMA':([6,9,10,11,15,16,17,19,20,21,],[13,-8,-11,-12,-7,-9,-10,-6,-9,-10,]),'EQUAL':([8,9,10,11,15,16,17,20,21,],[14,-8,-11,-12,-7,-9,-10,-9,-10,]),}
 
@@ -27,17 +27,17 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> head_expr","S'",1,None,None,None),
-  ('head_expr -> HTML elements_outside','head_expr',2,'p_head','parser.py',7),
-  ('elements_outside -> LSB elements_inside_comma RSB','elements_outside',3,'p_elements_outside','parser.py',12),
-  ('elements_inside_comma -> elements_inside_equal COMMA elements_inside_equal','elements_inside_comma',3,'p_elements_inside_comma1','parser.py',18),
-  ('elements_inside_comma -> elements_inside_equal','elements_inside_comma',1,'p_elements_inside_comma2','parser.py',24),
-  ('elements_inside_comma -> empty','elements_inside_comma',1,'p_elements_inside_comma2','parser.py',25),
-  ('elements_inside_equal -> attr_root EQUAL attr_root','elements_inside_equal',3,'p_elements_inside_equal','parser.py',31),
-  ('attr_root -> attr attr','attr_root',2,'p_attr0','parser.py',37),
-  ('attr_root -> attr','attr_root',1,'p_attr00','parser.py',42),
-  ('attr -> attr IDENTIFIER','attr',2,'p_attr1','parser.py',47),
-  ('attr -> attr OTHER','attr',2,'p_attr1','parser.py',48),
-  ('attr -> IDENTIFIER','attr',1,'p_attr2','parser.py',54),
-  ('attr -> OTHER','attr',1,'p_attr2','parser.py',55),
-  ('empty -> <empty>','empty',0,'p_empty','parser.py',61),
+  ('head_expr -> HTML elements_outside','head_expr',2,'p_head','parser.py',8),
+  ('elements_outside -> LSB elements_inside_comma RSB','elements_outside',3,'p_elements_outside','parser.py',14),
+  ('elements_inside_comma -> elements_inside_equal COMMA elements_inside_equal','elements_inside_comma',3,'p_elements_inside_comma1','parser.py',20),
+  ('elements_inside_comma -> elements_inside_equal','elements_inside_comma',1,'p_elements_inside_comma2','parser.py',26),
+  ('elements_inside_comma -> empty','elements_inside_comma',1,'p_elements_inside_comma2','parser.py',27),
+  ('elements_inside_equal -> attr_root EQUAL attr_root','elements_inside_equal',3,'p_elements_inside_equal','parser.py',33),
+  ('attr_root -> attr attr','attr_root',2,'p_attr0','parser.py',39),
+  ('attr_root -> attr','attr_root',1,'p_attr00','parser.py',44),
+  ('attr -> attr IDENTIFIER','attr',2,'p_attr1','parser.py',49),
+  ('attr -> attr OTHER','attr',2,'p_attr1','parser.py',50),
+  ('attr -> IDENTIFIER','attr',1,'p_attr2','parser.py',56),
+  ('attr -> OTHER','attr',1,'p_attr2','parser.py',57),
+  ('empty -> <empty>','empty',0,'p_empty','parser.py',63),
 ]

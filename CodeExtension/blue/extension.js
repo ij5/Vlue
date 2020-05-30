e7e88fcd4a03c7d22606862e21d9b8bb8454924c
jhlee838 <jhlee838@gmail.com>
2020-05-30 14:42:53 +0900

    update files

--- CodeExtension/blue/extension.js
+++ CodeExtension/blue/extension.js
@@ -21,7 +21,7 @@ function activate(context) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Blue Language!');
+		vscode.window.showInformationMessage('Hello World from Blue Language!!!');
 	});
 
 	context.subscriptions.push(disposable);

43fd6bd367f1682993ebd5c413e3fbc2b9260cc2
jhlee838 <jhlee838@gmail.com>
2020-05-31 13:38:55 +0900

    Update main.py

--- src/g3/main.py
+++ src/g3/main.py
@@ -524,8 +524,9 @@ def p_inside_root_2(t):
 
 def p_inside_root_3(t):
     '''
-    inside DOT IDENTIFIER LSB empty RSB
+    inside : DOT IDENTIFIER LSB empty RSB
     '''
+    t[0] = t[1] + t[2] + t[3] + "" + t[5]
 
 def p_inside_root_4(t):
     '''

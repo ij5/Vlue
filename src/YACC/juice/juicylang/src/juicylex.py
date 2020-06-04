9ac468b9be9bee0f4d240671bc0785d051970094
jhlee838 <jhlee838@gmail.com>
2020-06-04 21:43:29 +0900

    update

---
+++ src/YACC/juice/juicylang/src/juicylex.py
@@ -0,0 +1,197 @@
+"""
+juicylang - Juicy programming language written in Python using PLY
+Author : Hamidreza Mahdavipanah
+Version: 1.0.0
+Repository: http://github.com/mahdavipanah/juicylang
+License : MIT License
+"""
+from colorama import init as colorama_init, Fore
+from re import UNICODE as RE_UNICODE
+import ply.lex as lex
+
+# Initialize colorama
+colorama_init(autoreset=True)
+
+
+# Computes column
+#     t is a token instance
+def find_column(t):
+    last_cr = t.lexer.lexdata.rfind('\n', 0, t.lexpos)
+    if last_cr < 0:
+        last_cr = 0
+    column = (t.lexpos - last_cr) + 1
+    return column
+
+
+# Prints an error output for illegal token
+def token_error(t):
+    print(Fore.RED + ("Illegal character '" + Fore.BLACK + "{}" + Fore.RED + "' in line {} column {}").format(
+        t.value[0], t.lineno, find_column(t))
+          )
+    t.lexer.skip(1)
+
+
+# Track line numbers
+def token_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+# Juicy's reserved keywords
+reserved = {
+    'if': 'IF',
+    'else': 'ELSE',
+    'while': 'WHILE',
+    'input': 'INPUT',
+    'print': 'PRINT',
+    'len': 'LEN',
+    'to_str': 'TO_STR',
+    'err': 'ERR',
+    'to_int': 'TO_INT',
+    'to_float': 'TO_FLOAT',
+    'and': 'AND',
+    'or': 'OR',
+    'not': 'NOT',
+    'bool': "BOOL",
+    'true': 'TRUE',
+    'false': 'FALSE',
+}
+
+# Juicy's tokens
+tokens = (
+             'NOTEQUAL',
+             'GE',
+             'LE',
+             'ASSIGN',
+             'SYMBOL',
+             'FLOAT',
+             'INT',
+             'STRING',
+         ) + tuple(reserved.values())
+
+t_NOTEQUAL = r'<>'
+t_ASSIGN = r':='
+
+literals = (
+    '>',
+    '<',
+    '=',
+    '+',
+    '-',
+    '*',
+    '/',
+    '^',
+    '{',
+    '}',
+    '[',
+    ']',
+    '(',
+    ')',
+    ':',
+    ';',
+    ',',
+)
+
+# Juicy's lexer states
+states = (
+    ('comment', 'exclusive'),
+)
+
+
+t_GE = r'>='
+t_LE = r'<='
+
+
+# Opening of first comment
+def t_LCOMM(t):
+    r'\/\*'
+    t.lexer.comment_level = 1
+    t.lexer.begin('comment')
+
+
+# Opening of first nested comment
+def t_comment_LCOMM(t):
+    r'\/\*'
+    t.lexer.comment_level += 1
+
+
+# Ignore everything inside a comment
+t_comment_ignore_contents = r'[\s\S]'
+# Just for removing PLY's warning
+t_comment_ignore = ''
+
+
+# Closing a comment
+def t_comment_RCOMM(t):
+    r'\*\/'
+    t.lexer.comment_level -= 1
+
+    # If it was the most outer comment closing
+    if t.lexer.comment_level == 0:
+        t.lexer.begin('INITIAL')
+
+
+t_comment_newline = token_newline
+
+t_comment_error = token_error
+
+
+# Read in a float. This rule has to be done before the int rule.
+def t_FLOAT(t):
+    r'\d+\.\d*(e-?\d+)?'
+    t.value = float(t.value)
+    return t
+
+
+# Read in an int
+def t_INT(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+
+# Read in a string, as in C. The following backslash sequences have their usual special meaning: \", \\, \n, and \t
+def t_STRING(t):
+    r'\"([^\\"]|(\\.))*\"'
+    escaped = 0
+    # Remove first and last double quotations
+    old_str = t.value[1:-1]
+    new_str = ""
+    for i in range(0, len(old_str)):
+        c = old_str[i]
+        if escaped:
+            if c == "n":
+                c = "\n"
+            elif c == "t":
+                c = "\t"
+            new_str += c
+            escaped = 0
+        else:
+            if c == "\\":
+                escaped = 1
+            else:
+                new_str += c
+    t.value = new_str
+    return t
+
+
+# Read in a symbol. This rule must be practically last since there are so
+# few rules concerning what constitutes a symbol
+def t_SYMBOL(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    # See if symbol is a reserved keyword
+    t.type = reserved.get(t.value, 'SYMBOL')
+    return t
+
+
+t_newline = token_newline
+
+t_ignore = ' \t'
+
+t_error = token_error
+
+# Build the lexer
+lex.lex(reflags=RE_UNICODE, errorlog=lex.NullLogger())
+
+if __name__ == '__main__':
+    lex.runmain()
---
+++ src/YACC/juice/juicylang/src/juicyyacc.py
@@ -0,0 +1,676 @@
+"""
+juicylang - Juicy programming language written in Python using PLY
+Author : Hamidreza Mahdavipanah
+Version: 1.0.0
+Repository: http://github.com/mahdavipanah/juicylang
+License : MIT License
+"""
+from colorama import init as colorama_init, Fore
+import ply.yacc as yacc
+from .juicylex import tokens
+
+
+# Computes column
+#     p is a production rule's stack
+#     i is the number of item in p
+def find_column(p, i):
+    last_cr = p.lexer.lexdata.rfind('\n', 0, p.lexpos(i))
+    if last_cr < 0:
+        last_cr = 0
+    column = (p.lexpos(i) - last_cr) + 1
+    return column
+
+
+# Check types for operator and operands
+def check_int_float_operands(p):
+    # If something is none then it's error has been reported
+    if p[1] is None or p[3] is None:
+        return False
+
+    if (type(p[1]) not in {int, float}) or (type(p[3]) not in {int, float}):
+        print(Fore.YELLOW + ("Operator '" + Fore.BLACK + p[2] +
+                             Fore.YELLOW + "' is only defined for 'int' and 'float' in line {} column {}").format(
+            p.lineno(2), find_column(p, 2))
+              )
+        return False
+
+    return True
+
+
+def boolexpr(expr):
+    if expr is None:
+        return None
+
+    if type(expr) is str:
+        return True if len(expr) > 0 else False
+
+    return True if expr != 0 else False
+
+
+# Initialize colorama
+colorama_init(autoreset=True)
+
+# A map that contains variables and their values
+variables = {}
+# Contains the last error number happened in runtime environment
+runtime_err = 0
+# A stack containing running states
+# It gets used for disabling interpreting in if and while statements
+running = [True]
+
+
+# Get's a symbol from variables dictionary and shows error if not exists
+def get_symbol(p, i):
+    # Get symbol's value form variables dictionary
+    symbol_val = variables.get(p[i], None)
+    # If symbol does not exist
+    if symbol_val is None:
+        print(Fore.RED + "Variable '" + Fore.BLACK + p[1] + Fore.RED
+              + "' is not defined in line {} column {}".format(p.lineno(1), find_column(p, 1)))
+    else:
+        return symbol_val
+
+    return None
+
+
+precedence = (
+    ('right', 'ASSIGN'),
+    ('left', '+', '-'),
+    ('left', '*', '/'),
+    ('left', '^'),
+    ('right', 'UMINUS'),  # Unary minus operator
+    ('left', 'OR'),
+    ('left', 'AND'),
+    ('right', 'NOT'),
+    ('right', 'BOOL'),
+)
+
+
+# Program's starting point
+def p_s(p):
+    """s : '{' p '}'"""
+    pass
+
+
+# Program's main body
+def p_p(p):
+    """
+    p : stmt p
+      | expr ';' p
+      | boolexpr ';' p
+      |
+    """
+    pass
+
+
+def p_stmt(p):
+    """
+    stmt : PRINT '(' print_arguments ')' ';'
+    """
+    if not running[-1]:
+        return
+
+    if p[3] is not None:
+        print(p[3])
+
+
+def p_print_arguments(p):
+    """
+    print_arguments : expr ',' print_arguments
+                    | boolexpr ',' print_arguments
+                    | boolexpr
+                    | expr
+    """
+    if not running[-1]:
+        return
+
+    if p[1] is None:
+        return
+
+    # Second rule
+    if len(p) == 2:
+        p[0] = str(p[1])
+    # First rule
+    else:
+        if p[3] is not None and p[1] is not None:
+            p[0] = str(p[1]) + p[3]
+
+
+def p_stmt_if(p):
+    """
+    stmt : IF '(' boolexpr ')' '{' ifA p ifB '}'
+         | IF '(' boolexpr ')' '{' ifA p ifB '}' ELSE '{' ifC p ifB '}'
+    """
+    pass
+
+
+def p_ifA(p):
+    """
+    ifA :
+    """
+    global running
+    # If current state is not running
+    if not running[-1]:
+        running.append(False)
+    # Push evaluated value as running state
+    else:
+        running.append(p[-3])
+
+
+def p_ifB(p):
+    """
+    ifB :
+    """
+    global running
+    running.pop()
+
+
+def p_ifC(p):
+    """
+    ifC :
+    """
+    global running
+    # If current state is not running
+    if not running[-1]:
+        running.append(False)
+    # Push not(evaluated value) as running state
+    else:
+        running.append(not p[-9])
+
+
+def p_stmt_while(p):
+    """
+    stmt : WHILE '(' boolexpr ')' '{' whileA p whileB '}'
+    """
+    pass
+
+
+def p_whileA(p):
+    """
+    whileA :
+    """
+    global running
+    # If current state is not running
+    if not running[-1]:
+        running.append(False)
+    # Push evaluated value as running state
+    else:
+        running.append(p[-3])
+
+
+def p_whileB(p):
+    """
+    whileB :
+    """
+    global running
+    running.pop()
+
+    if running[-1] and p[-5]:
+        p.lexer.lexpos = parser.symstack[-7].lexpos
+
+
+def p_boolexpr_and(p):
+    """
+    boolexpr : boolexpr AND boolexpr
+    """
+    if p[1] is not None and p[3] is not None:
+        p[0] = p[1] and p[3]
+
+
+def p_boolexpr_or(p):
+    """
+    boolexpr : boolexpr OR boolexpr
+    """
+    if not running[-1]:
+        return
+
+    if p[1] is not None and p[3] is not None:
+        p[0] = p[1] or p[3]
+
+
+def p_boolexpr_not(p):
+    """
+    boolexpr : NOT boolexpr
+    """
+    if not running[-1]:
+        return
+
+    if p[2] is not None:
+        p[0] = not p[2]
+
+
+def p_boolexpr_bool(p):
+    """
+    boolexpr : BOOL expr
+    """
+    if not running[-1]:
+        return
+
+    p[0] = boolexpr(p[2])
+
+
+def p_boolexpr_bool_boolexpr(p):
+    """
+    boolexpr : BOOL boolexpr
+    """
+    if not running[-1]:
+        return
+
+    p[0] = p[2]
+
+
+def p_boolexpr_not_expr(p):
+    """
+    boolexpr : NOT expr
+    """
+    if not running[-1]:
+        return
+
+    expr_bool = boolexpr(p[2])
+    if expr_bool is not None:
+        p[0] = not expr_bool
+
+
+def p_boolexpr_paran(p):
+    """
+    boolexpr : '(' boolexpr ')'
+    """
+    if not running[-1]:
+        return
+
+    if p[2] is not None:
+        p[0] = p[2]
+
+
+def p_boolexpr_true_false(p):
+    """
+    boolexpr : TRUE
+             | FALSE
+    """
+    if not running[-1]:
+        return
+
+    p[0] = True if p[1] == 'true' else False
+
+
+def p_boolexpr_comparison(p):
+    """
+    boolexpr : comparison
+    """
+    if not running[-1]:
+        return
+
+    if p[1] is None:
+        return
+
+    p[0] = True if type(p[1]) is not bool else False
+
+
+def p_comparison_gt(p):
+    """
+    comparison : comparisonA '>' expr
+    """
+    if not running[-1]:
+        return
+
+    if p[1] is None or p[3] is None:
+        p[0] = None
+    else:
+        if type(p[1]) is bool:
+            p[0] = False
+        else:
+            p[0] = p[3] if p[1] > p[3] else False
+
+
+def p_comparison_lt(p):
+    """
+    comparison : comparisonA '<' expr
+    """
+    if not running[-1]:
+        return
+
+    if p[1] is None or p[3] is None:
+        p[0] = None
+    else:
+        if type(p[1]) is bool:
+            p[0] = False
+        else:
+            p[0] = p[3] if p[1] < p[3] else False
+
+
+def p_comparison_eq(p):
+    """
+    comparison : comparisonA '=' expr
+    """
+    if not running[-1]:
+        return
+
+    if p[1] is None or p[3] is None:
+        p[0] = None
+    else:
+        if type(p[1]) is bool:
+            p[0] = False
+        else:
+            p[0] = p[3] if p[1] == p[3] else False
+
+
+def p_comparison_neq(p):
+    """
+    comparison : comparisonA NOTEQUAL expr
+    """
+    if not running[-1]:
+        return
+
+    if p[1] is None or p[3] is None:
+        p[0] = None
+    else:
+        if type(p[1]) is bool:
+            p[0] = False
+        else:
+            p[0] = p[3] if p[1] != p[3] else False
+
+
+def p_comparison_le(p):
+    """
+    comparison : comparisonA LE expr
+    """
+    if not running[-1]:
+        return
+
+    if p[1] is None or p[3] is None:
+        p[0] = None
+    else:
+        if type(p[1]) is bool:
+            p[0] = False
+        else:
+            p[0] = p[3] if p[1] <= p[3] else False
+
+
+def p_comparison_ge(p):
+    """
+    comparison : comparisonA GE expr
+    """
+    if not running[-1]:
+        return
+
+    if p[1] is None or p[3] is None:
+        p[0] = None
+    else:
+        if type(p[1]) is bool:
+            p[0] = False
+        else:
+            p[0] = p[3] if p[1] >= p[3] else False
+
+
+def p_comparisonA(p):
+    """
+    comparisonA : comparison
+                | expr
+    """
+    if not running[-1]:
+        return
+
+    p[0] = p[1]
+
+
+def p_expr_plus(p):
+    """
+    expr : expr '+' expr
+    """
+    if not running[-1]:
+        return
+
+    if p[1] is not None and p[3] is not None:
+        p[0] = p[1] + p[3]
+
+
+def p_expr_minus(p):
+    """
+    expr : expr '-' expr
+    """
+    if not running[-1]:
+        return
+
+    if check_int_float_operands(p):
+        p[0] = p[1] - p[3]
+
+
+def p_expr_mul(p):
+    """
+    expr : expr '*' expr
+    """
+    if not running[-1]:
+        return
+
+    if check_int_float_operands(p):
+        p[0] = p[1] * p[3]
+
+
+def p_expr_div(p):
+    """
+    expr : expr '/' expr
+    """
+    if not running[-1]:
+        return
+
+    if check_int_float_operands(p):
+        p[0] = p[1] / p[3]
+
+
+def p_expr_pow(p):
+    """
+    expr : expr '^' expr
+    """
+    if not running[-1]:
+        return
+
+    if check_int_float_operands(p):
+        p[0] = pow(p[1], p[3])
+
+
+def p_expr_symbol(p):
+    """
+    expr : SYMBOL
+    """
+    if not running[-1]:
+        return
+
+    # Get symbol's value
+    sym_val = get_symbol(p, 1)
+    # If symbol does not exist
+    if sym_val is not None:
+        p[0] = sym_val
+
+
+def p_expr(p):
+    """
+    expr : INT
+         | FLOAT
+         | STRING
+    """
+    if not running[-1]:
+        return
+
+    p[0] = p[1]
+
+
+def p_expr_paran(p):
+    """
+    expr : '(' expr ')'
+    """
+    if not running[-1]:
+        return
+
+    p[0] = p[2]
+
+
+def p_expr_unary_minus(p):
+    """
+    expr : '-' expr %prec UMINUS
+    """
+    if not running[-1]:
+        return
+
+    if p[2] is not None:
+        p[0] = -p[2]
+
+
+def p_expr_len(p):
+    """
+    expr : LEN '(' expr ')'
+    """
+    if not running[-1]:
+        return
+
+    if p[3] is not None:
+        if type(p[3]) is str:
+            p[0] = len(p[3])
+        else:
+            print(Fore.RED + "'len' only accepts string in line {} column {}".format(p.lineno(1), find_column(p, 1)))
+
+
+def p_expr_to_str(p):
+    """
+    expr : TO_STR '(' expr ')'
+    """
+    if not running[-1]:
+        return
+
+    if p[3] is not None:
+        p[0] = str(p[3])
+
+
+def p_expr_err(p):
+    """
+    expr : ERR '(' ')'
+    """
+    global runtime_err
+
+    if not running[-1]:
+        return
+
+    p[0] = runtime_err
+
+
+def p_expr_to_int(p):
+    """
+    expr : TO_INT '(' expr ')'
+    """
+    global runtime_err
+
+    if not running[-1]:
+        return
+
+    if p[3] is not None:
+        try:
+            p[0] = int(p[3])
+        except ValueError:
+            runtime_err = 1
+        else:
+            runtime_err = 0
+
+
+def p_expr_to_float(p):
+    """
+    expr : TO_FLOAT '(' expr ')'
+    """
+    global runtime_err
+
+    if not running[-1]:
+        return
+
+    if p[3] is not None:
+        try:
+            p[0] = float(p[3])
+        except ValueError:
+            runtime_err = 1
+        else:
+            runtime_err = 0
+
+
+def p_expr_input(p):
+    """
+    expr : INPUT '(' expr ')'
+         | INPUT '(' ')'
+    """
+    if not running[-1]:
+        return
+
+    prompt = str(p[3]) if len(p) > 4 else ''
+    p[0] = input(prompt)
+
+
+def p_expr_str_index(p):
+    """
+    expr : expr '[' expr ']'
+    """
+    if not running[-1]:
+        return
+
+    if p[1] is None or p[2] is None:
+        return
+
+    if type(p[1]) is not str:
+        print(Fore.RED + "Value is not indexable in line {} column {}".format(p.lineno(2), find_column(p, 2)))
+    else:
+        if type(p[3]) is not int:
+            print(Fore.RED + "Index is not 'int' in line {} column {}".format(p.lineno(4), find_column(p, 4)))
+        # Everything is ok
+        else:
+            # Check if index is out of range
+            if p[3] >= len(p[1]):
+                print(Fore.RED + "Index is out of range in line {} column {}".format(p.lineno(4), find_column(p, 4)))
+            else:
+                p[0] = p[1][p[3]]
+
+
+def p_expr_assign(p):
+    """
+    expr : SYMBOL ASSIGN expr
+    """
+    if not running[-1]:
+        return
+
+    if p[3] is not None:
+        variables[p[1]] = p[3]
+        p[0] = p[3]
+
+
+def p_expr_str_subscript(p):
+    """
+    expr : expr '[' expr_or_empty ':' expr_or_empty ']'
+    """
+    if not running[-1]:
+        return
+
+    if p[1] is None:
+        return
+
+    if type(p[1]) is not str:
+        print(Fore.RED + "Value is not subscriptable in line {} column {}".format(p.lineno(2), find_column(p, 2)))
+    else:
+        if p[3] is not None and type(p[3]) is not int:
+            print(Fore.RED + "Index is not 'int' in line {} column {}".format(p.lineno(2), find_column(p, 2)))
+            return
+        if p[5] is not None and type(p[5]) is not int:
+            print(Fore.RED + "Index is not 'int' in line {} column {}".format(p.lineno(4), find_column(p, 4)))
+            return
+        # Everything is ok
+        else:
+            p[0] = p[1][p[3]:p[5]]
+
+
+def p_expr_or_empty(p):
+    """
+    expr_or_empty : expr
+                  |
+    """
+    if not running[-1]:
+        return
+
+    if len(p) == 2:
+        p[0] = p[1]
+
+
+# Build the parser
+parser = yacc.yacc(errorlog=yacc.NullLogger())

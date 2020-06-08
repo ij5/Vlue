a4c47cc16b1209b8cd1db208ff26f78533b76901
jhlee838 <jhlee838@gmail.com>
2020-06-08 20:24:38 +0900

    Update AST.py

--- src/g3/AST.py
+++ src/g3/AST.py
@@ -1,8 +1,17 @@
 import ast
 
-node = ast.FunctionDef('function', ['a'], 'print("Hello World!")')
+node = ast.Add()
 node.lineno = 0
 node.col_offset = 0
+
+node = ast.BinOp(1, node, 2)
+node.lineno = 0
+node.col_offset = 0
+
+node = ast.Expr(node)
+node.lineno = 0
+node.col_offset = 0
+
 node = ast.Module([node])
 code = compile(node, '<string>', 'exec')
 print(ast.dump(node))

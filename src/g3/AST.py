79d9939db992c18984ab09a3a4adb9ad59e95e9e
jhlee838 <jhlee838@gmail.com>
2020-06-08 20:12:17 +0900

    Update AST.py

--- src/g3/AST.py
+++ src/g3/AST.py
@@ -1,7 +1,10 @@
 import ast
-ast.Num()
-node = ast.UnaryOp(ast.Constant(1), ast.Constant(2))
 
+node = ast.FunctionDef('function', ['a'], 'print("Hello World!")')
+node.lineno = 0
+node.col_offset = 0
+node = ast.Module([node])
+code = compile(node, '<string>', 'exec')
 print(ast.dump(node))
 
 #compile(a, '<string>', 'exec')

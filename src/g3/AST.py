744cf79969fde0d4c7fb99d2c56e1c97da7d88c7
jhlee838 <jhlee838@gmail.com>
2020-06-08 19:48:48 +0900

    Update AST.py

--- src/g3/AST.py
+++ src/g3/AST.py
@@ -1,8 +1,7 @@
 import ast
+ast.Num()
+node = ast.UnaryOp(ast.Constant(1), ast.Constant(2))
 
-a = ast.FunctionDef('test', 'a,b', 'print("Hello World!")')
-b = ast.Call('test')
-c = ast.NodeTransformer()
-print(ast.dump(a))
-print(ast.dump(b))
-compile(a, '<string>', 'exec')
+print(ast.dump(node))
+
+#compile(a, '<string>', 'exec')

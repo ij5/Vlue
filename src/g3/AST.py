532f8823c4193feff3a0113d8c7b0728ce121bfc
jhlee838 <jhlee838@gmail.com>
2020-06-08 21:16:59 +0900

    Update AST.py

--- src/g3/AST.py
+++ src/g3/AST.py
@@ -1,19 +1,3 @@
 import ast
 print(ast)
 
-a = ast.parse("1+1")
-print(ast.dump(a))
-
-node = ast.BinOp(left=ast.Num(1), op=ast.Add(), right=ast.Num(1))
-node.lineno = 1
-node.col_offset = -1
-
-node = ast.Expr(node)
-node.lineno = 1
-node.col_offset = -1
-
-node = ast.Module(body=[node])
-node.lineno = 1
-node.col_offset = -1
-
-node = compile(node, '<string>', 'exec')

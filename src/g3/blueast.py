080b00f0d95ba4d103d0890a1fc106e3776ce4a6
jhlee838 <jhlee838@gmail.com>
2020-06-10 21:32:10 +0900

    update

--- src/g3/blueast.py
+++ src/g3/blueast.py
@@ -1,15 +1,10 @@
 from ast import *
 
-class Expr(Module):
-    def __init__(self):
-        pass
+class Transformer(NodeTransformer):
+    pass
+a = "hello"
 
+node = parse('1.2+3.4')
+Transformer.visit(node)
+dump(node)
 
-class BinOp(Expr):
-    def __init__(self, left, op, right):
-        self.left = left
-        self.right = right
-        self.op = op
-
-a = BinOp(Constant(value=8), Add(), Constant(value=3))
-print(dump(a))

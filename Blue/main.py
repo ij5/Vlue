a3a283e684db6fc183f597f8cb30568e320e2545
jhlee838 <jhlee838@gmail.com>
2020-06-24 20:20:18 +0900

    Update main.py

--- Blue/main.py
+++ Blue/main.py
@@ -1371,7 +1371,10 @@ def p_list(t):
     t[0].VALUE = List(elts=[Num(n=1), Num(n=2), Num(n=3)], ctx=Load())
 
 def p_list_params(t):
-    '''list_params : '''
+    '''list_params : list_p COMMA list_p'''
+
+def p_list_p(t):
+    '''list_p : '''
 
 ################### CALCULATE
 

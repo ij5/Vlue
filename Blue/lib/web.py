faba22ba9ef66a7bc47869c95d58015cc052b69c
jhlee838 <jhlee838@gmail.com>
2020-08-10 20:42:17 +0900

    web

--- Blue/lib/web.py
+++ Blue/lib/web.py
@@ -1,2 +1,7 @@
 from http import server
+import os
 
+def start():
+    os.system("cd {}".format(os.path.join(os.path.dirname(os.path.abspath(__file__)), "server")))
+    result = os.popen("dir").read()
+    print(result)

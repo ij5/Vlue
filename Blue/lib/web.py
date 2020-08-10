7549be53920127c77756de576c39c58993b876e8
jhlee838 <jhlee838@gmail.com>
2020-08-10 21:12:52 +0900

    added port

--- Blue/lib/web.py
+++ Blue/lib/web.py
@@ -1,7 +1,7 @@
 from http import server
 import os
 
-def start():
+def start(port=8000):
     os.system("cd {}".format(os.path.join(os.path.dirname(os.path.abspath(__file__)), "server")))
     result = os.popen("dir").read()
     print(result)

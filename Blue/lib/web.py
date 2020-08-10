4fc94e848f2ff0214388cda210c5f768be2b109d
jhlee838 <jhlee838@gmail.com>
2020-08-10 23:23:57 +0900

    update

--- Blue/lib/web.py
+++ Blue/lib/web.py
@@ -3,9 +3,5 @@ import subprocess
 import os
 
 def start(port=8000):
-    ex ="cd {}".format(os.path.join(os.path.dirname(os.path.abspath(__file__)), "server"))
-    print(ex)
-    subprocess.Popen(ex).communicate()
-    print(ex)
-    # result = subprocess.Popen("dir").communicate()
-    # print(result)
+    subprocess.Popen("nginx", shell=True, cwd="nginx")
+    

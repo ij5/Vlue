d17ae5f05b4dbde9ad65312271e1d9bb420823a9
jhlee838 <jhlee838@gmail.com>
2020-08-11 16:09:28 +0900

    CHECKPOINT

--- Blue/lib/web.py
+++ Blue/lib/web.py
@@ -2,6 +2,11 @@ from http import server
 import subprocess
 import os
 
-def start(port=8000):
+def _start(port=8000):
     subprocess.Popen("nginx", shell=True, cwd="nginx")
-    
+
+def start(port=8000):
+    try:
+        _start(port)
+    except KeyboardInterrupt:
+        subprocess.Popen("nginx -s quit")

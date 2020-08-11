831de342c491c03d9b37fb665bd2be379d8ac2a3
jhlee838 <jhlee838@gmail.com>
2020-08-12 00:21:48 +0900

    update

--- Blue/lib/web.py
+++ Blue/lib/web.py
@@ -1,11 +1,17 @@
 import subprocess
 import atexit
 import lib.NGINXConf as conf
+import os
 
 def start(port=8000):
     c = conf.Conf()
     server = conf.Server()
-    
+    server.add(
+        conf.Key('listen', '80'),
+        conf.Comment("Hello World!"),
+        conf.Key('server_name', '127.0.0.1'),
+        conf.Key('root', )
+    )
     subprocess.Popen("nginx", shell=True, cwd="nginx")
 
 def onexit():

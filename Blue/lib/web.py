cc2aafa971a44c8d0d0d4ec0e4b4fe9b9782f70c
jhlee838 <jhlee838@gmail.com>
2020-08-13 20:45:56 +0900

    Update web.py

--- Blue/lib/web.py
+++ Blue/lib/web.py
@@ -1,30 +1,35 @@
 import subprocess
 import atexit
 import lib.NGINXConf as conf
-import os
 import platform
 
-def start(port=8000):
-    if(platform.system()=="Windows"):
-        c = conf.Conf()
-        server = conf.Server()
-        server.add(
-            conf.Key('listen', '80'),
-            conf.Comment("Hello World!"),
-            conf.Key('server_name', '127.0.0.1'),
-            conf.Key('root', )
-        )
-        subprocess.Popen("nginx", shell=True, cwd="nginx")
-    elif(platform.system()=="Linux"):
+def start(domain="localhost", port=8000):
+    if(platform.system()=='Windows'):
         c = conf.Conf()
         server = conf.Server()
         server.add(
             conf.Key('listen', '80'),
             conf.Key('listen', '[::]:80'),
             conf.Comment("Hello World!"),
-        )
-        result = subprocess.Popen("nginx", shell=True, cwd="nginx").communicate(),
+            conf.Key('server_name', domain),
+            conf.Key('include', '../security.conf'),
 
+        )
+        subprocess.Popen("nginx", shell=True, cwd="nginx")
+    elif(platform.system()=='Linux'):
+        c = conf.Conf()
+        server = conf.Server()
+        server.add(
+            conf.Key('listen', '80'),
+            conf.Key('listen', '[::]:80'),
+            conf.Comment("Hello World!"),
+            conf.Key('server_name', domain),
+            conf.Key('include', '/etc/nginx/security.conf'),
+            conf.Location(
+                '/',
+                conf.Key('include', '/etc/nginx/python_uwsgi.conf'),
+            )
+        )
 
 def onexit():
     subprocess.Popen("nginx -s quit", shell=True, cwd="nginx")

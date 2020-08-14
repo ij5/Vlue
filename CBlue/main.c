d120bc83c066abf99e5329266793051d5b962c5e
jhlee838 <jhlee838@gmail.com>
2020-08-14 22:46:15 +0900

    update

--- CBlue/main.c
+++ CBlue/main.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+#define OWL_PARSER_IMPLEMENTATION
+#include "parser.h"
+
+int main(void){
+    struct owl_tree *tree;
+    tree = owl_tree_create_from_file(stdin);
+    owl_tree_print(tree);
+    owl_tree_destroy(tree);
+    return 0;
+}

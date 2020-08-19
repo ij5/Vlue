cf1ca3b5a7d9316f099a3f056c2c95fc22bf85b6
jhlee838 <jhlee838@gmail.com>
2020-08-19 16:17:23 +0900

    update cblue

--- CBlue/main.c
+++ CBlue/main.c
@@ -1,14 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct _Person{
+    char name[20];
+    int age;
+    char address[100];
+}Person;
+
+enum{
+    A = 0,
+    B,
+    C,
+    D,
+    E
+};
 
 int main(void){
-    int num1 = 100;
-
-    int *numPtr = &num1;
-
-    printf("%d", *numPtr);
     
+    int num = 5;
+
+    int *numPtr = &num;
+
+    printf("%d\n", *numPtr);
 
     return 0;
 }

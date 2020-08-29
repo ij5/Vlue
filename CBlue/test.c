3e1fce5515f55500874d1b24a20465f36ee3e409
이재희 <55607202+jhlee838@users.noreply.github.com>
2020-08-29 17:30:11 +0900

    Update test.c

--- CBlue/test.c
+++ CBlue/test.c
@@ -274,7 +274,13 @@ void initChunk(Chunk *chunk){
 }
 
 void writeChunk(Chunk *chunk){
-    
+    if(chunk->capacity < chunk->count + 1){
+        int oldCapacity = chunk->capacity;
+        chunk->capacity = GROW_CAPACITY(oldCapacity);
+        chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+    }
+    chunk->code[chunk->count] = byte;
+    chunk->count++;
 }
 
 

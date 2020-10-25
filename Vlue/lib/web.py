d404003ae067d92cbb64696ef680aeb5df91c445
이재희 <55607202+jhlee838@users.noreply.github.com>
2020-10-25 22:12:21 +0900

    Update web.py

--- Vlue/lib/web.py
+++ Vlue/lib/web.py
@@ -3,39 +3,17 @@ import atexit
 import lib.NGINXConf as conf
 import platform
 import flask
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
-class Web:
+class Web(BaseHTTPRequestHandler):
     def __init__():
         self.port = 8000;
         self.host = 'localhost'
+        
     def start():
-        if(platform.system()=='Windows'):
-            c = conf.Conf()
-            server = conf.Server()
-            server.add(
-                conf.Key('listen', '80'),
-                conf.Key('listen', '[::]:80'),
-                conf.Comment("Hello World!"),
-                conf.Key('server_name', self.host),
-                conf.Key('include', '../security.conf'),
-
-            )
-            subprocess.Popen("nginx.exe", shell=True, cwd="nginx")
-        elif(platform.system()=='Linux'):
-            c = conf.Conf()
-            server = conf.Server()
-            server.add(
-                conf.Key('listen', '80'),
-                conf.Key('listen', '[::]:80'),
-                conf.Comment("Hello World!"),
-                conf.Key('server_name', self.host),
-                conf.Key('include', '/etc/nginx/security.conf'),
-                conf.Location(
-                    '/',
-                    conf.Key('include', '/etc/nginx/python_uwsgi.conf'),
-            ),
+        pass
             
-    def start_dev(port=8000, debug=True):
+    def start_dev(self, port=8000, debug=True):
         app = flask.Flask(__name__)
         app.run(debug=debug,)
 

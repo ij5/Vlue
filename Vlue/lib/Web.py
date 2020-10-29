5b1ec42c43fb68aa248d9a83c2d7a798d1384037
이재희 <55607202+jhlee838@users.noreply.github.com>
2020-10-29 19:46:41 +0900

    update

--- Vlue/lib/Web.py
+++ Vlue/lib/Web.py
@@ -4,12 +4,27 @@ import lib.NGINXConf as conf
 import platform
 import flask
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
 
 class Web(BaseHTTPRequestHandler):
     def __init__(self):
         self.port = 8000;
         self.host = 'localhost'
-        
+
+    def _send_cors_headers(self):
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
+        self.send_header("Access-Control-Allow-headers", "x-api-key, Content-Type")
+
+    def _send_dict_resounse(self, d):
+        self.wfile(bytes(json.dumps(d), "utf8"))
+
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self._send_cors_headers()
+        self.send_headers()
+
     def start(self):
         pass
     
